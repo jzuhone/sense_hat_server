815d5266e8b3e15c405e1d3f5fb8f69e8c7d586c
John ZuHone <jzuhone@hoobs>
2020-11-16 10:23:45 -0500

    Fix errors

--- show.py
+++ show.py
@@ -1,7 +1,9 @@
 from sense_hat import SenseHat
 from datetime import datetime
 import requests
+from requests.exceptions import ConnectionError
 import numpy as np
+from math import fabs
 
 api_key = "77d264bda7cf84c6b0517683417e6d89"
 base_url = "http://api.openweathermap.org/data/2.5/weather?"
@@ -20,9 +22,12 @@ while True:
     temp = sense.get_temperature_from_pressure()
     temp = int(np.rint(temp*9.0/5.0+32))
     sense.show_message(str(temp), text_colour=[128,0,0])
-    response = requests.get(complete_url) 
-    x = response.json()
-    if x["cod"] != "404": 
-        y = x["main"]
-        temp2 = int(np.rint(9.0*(y["temp"] - 273.15)/5.0+32))
-        sense.show_message(str(temp2), text_colour=[0,128,0])
+    try:
+        response = requests.get(complete_url) 
+        x = response.json()
+        if x["cod"] != "404": 
+            y = x["main"]
+            temp2 = int(np.rint(9.0*(y["temp"] - 273.15)/5.0+32))
+    except ConnectionError:
+        temp2 = "NA"
+    sense.show_message(str(temp2), text_colour=[0,128,0])
